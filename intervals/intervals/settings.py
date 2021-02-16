8d2ab7701da3f7207629185d23f8890c5eda43c8
Jordan Jamali <jordanjjamali@gmail.com>
2021-02-16 03:27:05 -0500

    Add basic models and fields

--- intervals/intervals/settings.py
+++ intervals/intervals/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'intervalsApp.apps.IntervalsappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- intervals/intervalsApp/models.py
+++ intervals/intervalsApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Login(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+class User(models.Model):
+    level = models.IntegerField(default=0)
+    score = models.DecimalField(max_digits=5, decimal_places=2)
