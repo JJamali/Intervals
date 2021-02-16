8186a59cfed17a276117ac7f8bbe96d9ba517bd3
Jordan Jamali <jordanjjamali@gmail.com>
2021-02-16 16:20:06 -0500

    Add basic structural configuration

--- intervals/intervals/settings.py
+++ intervals/intervals/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ[SECRET_KEY]
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'intervalsApp.apps.IntervalsappConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
