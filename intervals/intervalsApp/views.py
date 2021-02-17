96b32f2b7acbbf724d22fef2d1e4dbf8a3e770e4
Jordan Jamali <jordanjjamali@gmail.com>
2021-02-16 23:35:57 -0500

    Clean up views.py

--- intervals/intervalsApp/views.py
+++ intervals/intervalsApp/views.py
@@ -3,8 +3,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from intervalsApp.models import User
 
+
 @api_view(['POST'])
-def create_user(request): # user is symbolic representation of current user being processed
+def create_user(request):  # user is symbolic representation of current user being processed
     if request.method == 'POST':
         username = request.data['username']
         user = User(username=username)
