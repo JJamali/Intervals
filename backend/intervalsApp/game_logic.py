f750c8f0ad1c77e54c1a10cec0fb4a7ff8c5a752
Jordan Jamali <jordanjjamali@gmail.com>
2021-04-06 21:50:10 -0400

    Fix bug in recent_results

--- backend/intervalsApp/game_logic.py
+++ backend/intervalsApp/game_logic.py
@@ -12,14 +12,14 @@ def handle_answer(user: User, correct):
         current_user.total_completed += 1
         # Cycle list data structure to contain most recent results
         if len(current_user.recent_results) >= apps.get_app_config('intervalsApp').SCORE_RANGE:
-            current_user.recent_results.pop()
+            current_user.recent_results.pop(0)
         current_user.recent_results.append(True)
 
     else:
         current_user.total_completed += 1
         # Cycle list data structure to contain most recent results
         if len(current_user.recent_results) >= apps.get_app_config('intervalsApp').SCORE_RANGE:
-            current_user.recent_results.pop()
+            current_user.recent_results.pop(0)
         current_user.recent_results.append(False)
 
     current_user.save()
