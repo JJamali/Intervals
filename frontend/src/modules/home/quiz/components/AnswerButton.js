0cefe64ffac925d8cdd005a7f23a2557d9219b1f
Lukas Boelling <>
2021-04-08 18:24:00 -0400

    Create progress bar component

--- frontend/src/modules/home/quiz/components/AnswerButton.js
+++ frontend/src/modules/home/quiz/components/AnswerButton.js
@@ -2,44 +2,31 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import { Button, Grid, ButtonBase } from "@material-ui/core";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
-//
-// const styles = theme => ({
-//     button: {
-//         root: {
-//             background: "red",
-//             "&$disabled": {
-//                 background: "blue",
-//             }
-//         },
-//     }
-// });
+
 
 const useStyles = makeStyles({
-    button: {
-    },
     disabled: {
-        background: props => props.isCorrect ? "green" : "red",
         "&$disabled": {
+            backgroundColor: props => props.isCorrect ? "green" : "red",
             color: "black"
-        }
+        },
     }
 })
 
-//const AnswerButton = Button;
 
 const AnswerButton = (props) => {
     const classes = useStyles(props);
 
     return (
-        <Button value={props.answer} onClick={props.onClick} disabled={props.answered} classes={{ root: classes.button, disabled: classes.disabled }}>
+        <Button
+            value={props.answer}
+            variant="contained"
+            onClick={props.onClick}
+            disabled={props.answered}
+            classes={{ disabled: classes.disabled }}>
             {props.answer}
         </Button>
     )
 }
-//
-// AnswerButton.propTypes = {
-//   classes: PropTypes.object.isRequired
-// };
-//
-// export default withStyles(styles)(AnswerButton);
+
 export default AnswerButton;
