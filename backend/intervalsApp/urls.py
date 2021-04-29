1a50c111eea835b98ef9c4cbda5c6d13ae362052
Jordan Jamali <jordanjjamali@gmail.com>
2021-04-28 22:18:48 -0400

    Create endpoint and view for updating user settings

--- backend/intervalsApp/urls.py
+++ backend/intervalsApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import current_user, UserList, question, answer_check, login_view, logout_view, global_stats
+from .views import current_user, UserList, question, answer_check, login_view, logout_view, global_stats, update_settings
 
 urlpatterns = [
     path('login/', login_view, name='login'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('users/', UserList.as_view()),
     path('question/', question, name='question'),
     path('answer_check/', answer_check, name='answer_check'),
-    path('global_stats/', global_stats, name='global_stats')
+    path('global_stats/', global_stats, name='global_stats'),
+    path('update_settings/', update_settings, name='update_settings')
 ]
