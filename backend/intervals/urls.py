5cc10dafc3af9bbdf9af5446b082937b74f5f44a
Jordan Jamali <jordanjjamali@gmail.com>
2021-05-01 15:59:23 -0400

    Remove jwt from backend

--- backend/intervals/urls.py
+++ backend/intervals/urls.py
@@ -14,15 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from rest_framework_simplejwt.views import TokenRefreshView
 from django.contrib import admin
 from django.urls import include, path
-from intervalsApp.serializers import MyTokenObtainPairView
 
 
 urlpatterns = [
-    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/intervals/', include('intervalsApp.urls')),  # Wire up API automatically
     path('admin/', admin.site.urls),
 ]
--- backend/intervalsApp/serializers.py
+++ backend/intervalsApp/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
 from .models import User, IntervalsProfile, RecentResults
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class RecentResultsSerializer(serializers.ModelSerializer):
@@ -36,54 +33,10 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         print(validated_data)
-        profile_data = validated_data.pop('profile')
         password = validated_data.pop('password')
         # Create user
         user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
-        # Create profile
-        IntervalsProfile.objects.create(user=user, level=0)
 
         return user
-
-
-class UserSerializerWithToken(serializers.ModelSerializer):
-    token = serializers.SerializerMethodField()
-    password = serializers.CharField(write_only=True)
-
-    def get_token(self, user):
-        refresh = RefreshToken.for_user(user)
-
-        return {
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        }
-
-    def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
-
-    class Meta:
-        model = User
-        fields = ('token', 'username', 'password')
-
-
-# Allows for extraction of other important information along with token e.g. user information
-# Encodes non-username/password information in the access token
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-
-    # Customize the JWT response (the response, not the token) to include user information
-    def validate(self, attrs):
-        data = super().validate(attrs)
-        data['user'] = UserSerializer(self.user).data
-
-        return data
-
-
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
