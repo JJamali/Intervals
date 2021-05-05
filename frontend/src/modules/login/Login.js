b8a83ac23d779727cadd49a2749e728902ce3697
Mahir Mir <m24mir@uwaterloo.ca>
2021-05-05 12:17:32 -0400

    Update login page

--- frontend/src/modules/login/Login.js
+++ frontend/src/modules/login/Login.js
@@ -2,14 +2,14 @@ import LoginForm from "./components/LoginForm.js";
 import React from "react";
 import {Grid} from "@material-ui/core";
 import { Link } from "react-router-dom";
-import Button from '@material-ui/core/Button'
-import Container from '@material-ui/core/Container'
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
-import {  ThemeProvider, createMuiTheme} from '@material-ui/core/styles'
-import Box from '@material-ui/core/Box'
-import { blue } from '@material-ui/core/colors'
-import Typography from '@material-ui/core/Typography'
-import { boxShadows } from '@material-ui/core/shadows'
+import {  makeStyles, ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import {blue, grey} from '@material-ui/core/colors';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 
 const theme = createMuiTheme({
     typography: {
@@ -19,42 +19,54 @@ const theme = createMuiTheme({
         h2: {
             fontSize: 18,
         },
+        h3: {
+            fontSize: 14,
+        },
     },
     palette: {
         primary: {
             main: blue[500]
+        },
+        secondary: {
+            main: grey[500]
         }
-    }
+    },
 })
+
+const useStyles = makeStyles ({
+    paper: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: 'blue',
+        elevation: '1',
+    },
+});
 
 
 export default function Login() {
+    const classes = useStyles();
+
     return (
-        <Box component = "span" m = {1} boxShadow ={3}>
-            <Container maxWidth = "xs">
-                <ThemeProvider theme = {theme}>
-                    <Grid style={{textAlign: "center"}}>
-                        <Typography variant = "h1">
-                            Let's get started!
-                        </Typography>
-                        <Typography variant = "h2">
-                            Sign in to your account.
-                        </Typography>
-                        <LoginForm />
-                        <Link to="/signup">No account? Sign up here</Link>
-                        <div>
-                        </div>
-                        <Button variant = "contained" color = "primary">
-                            Login
-                        </Button>
-                        <div>
-                        </div>
-                        <Button variant = "contained" color = "primary">
-                            Play as guest
-                        </Button>
-                    </Grid>
-                </ThemeProvider>
-            </Container>
+        <Box display = "flex" justifyContent = "center" alignItems = "center" minHeight = "90vh">
+            <Paper classname={classes.paper} elevation = {24}>
+                <Box display = "flex" justifyContent = "center" alignItems = "center" minHeight = "80vh" minWidth = "150vh">
+                    <Container maxWidth = "lg">
+                        <ThemeProvider theme = {theme}>
+                            <Grid style={{textAlign: "center"}}>
+                                <Typography variant = "h1">
+                                    Let's get started!
+                                </Typography>
+                                <Typography variant = "h2">
+                                    Sign in to your account.
+                                </Typography>
+                                <br></br>
+                                <LoginForm />
+                            </Grid>
+                        </ThemeProvider>
+                    </Container>
+                </Box>
+            </Paper>
         </Box>
+
     )
 }
--- frontend/src/modules/login/components/LoginForm.js
+++ frontend/src/modules/login/components/LoginForm.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { Redirect } from 'react-router-dom';
-import { TextField, Button } from "@material-ui/core";
+import {Link, Redirect} from 'react-router-dom';
+import { TextField, Button, Box } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
 import LoginFormBl from "./bl.js";
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(1),
+    },
+}));
+
 
 export default function LoginForm() {
+    const classes = useStyles();
     const { loggedIn, handleChange, handleSubmit, loginFailed } = LoginFormBl();
 
     return (
@@ -11,13 +21,28 @@ export default function LoginForm() {
             {loggedIn ? <Redirect to="/" /> :
             <form onSubmit={handleSubmit}>
                 <div>
-                    <TextField type="text" name="username" label="Username" onChange={handleChange} />
+                    <TextField id = "filled-basic" label = "Filled" variant = "filled" type="text" name="username" label="Username" onChange={handleChange} />
                 </div>
                 <div>
-                    <TextField type="password" name="password" label="Password" onChange={handleChange} />
+                    <TextField id = "filled-basic" label = "Filled" variant = "filled" type="password" name="password" label="Password" onChange={handleChange} />
                 </div>
-                <Button type="submit">Submit</Button>
+                <Typography variant = "h3" className = {classes.margin}>
+                    No account? Sign up
+                    <Link to="/signup"> here.</Link>
+                </Typography>
+                <br></br>
+                <Button type="submit" variant = "contained" color = "primary" className = {classes.margin} style = {{maxWidth: '150px', minWidth: '150px'}}>Login</Button>
                 {loginFailed && <p>Invalid login</p>}
+                <div>
+                </div>
+                <Button variant = "contained" color = "secondary" style = {{maxWidth: '150px', minWidth: '150px'}}>
+                    Play as guest
+                </Button>
+                <br></br>
+                <br></br>
+                <Typography variant = "h3">
+                    <Link to = "https://github.com/JJamali/Intervals"> View GitHub Repository</Link>
+                </Typography>
             </form>}
         </>
     );
