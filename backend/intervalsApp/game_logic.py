95a91570450357ae9ac06324add6a52e1b22807f
Jordan Jamali <jordanjjamali@gmail.com>
2021-06-21 18:50:03 -0400

    Refactor handle_answer

--- backend/intervalsApp/game_logic.py
+++ backend/intervalsApp/game_logic.py
@@ -8,20 +8,19 @@ def handle_answer(user: User, correct):
     current_user = user.profile
     recent_results = current_user.recent_results_at_level(current_user.current_level)
 
+    # Total completed is always updated
+    recent_results.total_completed += 1
+
+    # Cycle list data structure to contain most recent results
+    if len(recent_results.recent_results) >= SCORE_RANGE:
+        recent_results.recent_results.pop(0)
+
     # Update user model based on correctness
     if correct:
         recent_results.total_correct += 1
-        recent_results.total_completed += 1
-        # Cycle list data structure to contain most recent results
-        if len(recent_results.recent_results) >= SCORE_RANGE:
-            recent_results.recent_results.pop(0)
         recent_results.recent_results.append(True)
 
     else:
-        recent_results.total_completed += 1
-        # Cycle list data structure to contain most recent results
-        if len(recent_results.recent_results) >= SCORE_RANGE:
-            recent_results.recent_results.pop(0)
         recent_results.recent_results.append(False)
 
     # Level up user logic
