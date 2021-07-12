3a85087001f562df5cbbc0eca41c79458959be55
Lukas Boelling <lukasboelling002@gmail.com>
2021-07-12 00:30:51 -0400

    Add newline to end of file

--- backend/intervalsApp/management/commands/clearguests.py
+++ backend/intervalsApp/management/commands/clearguests.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.apps import apps
 from django.contrib.auth import get_user_model
 from datetime import datetime
